89ed3eb0c511e67b8accbf1f8855e0b3c0761e7b
2002fatmaalaa <fatmaaboukila@gmail.com>
2024-11-07 15:45:03 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript functionality here, such as smooth scrolling or interactive effects.
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to Fatma's Portfolio!");
+});
